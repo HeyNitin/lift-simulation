e4f993fbd999c541636325fb9c3021dad3fde205
Nitin Kalra <nikkalra88@gmail.com>
2022-10-03 00:30:05 +0530

    feat: Added dynamic floor creation

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.getElementById("form");
+const layout = document.getElementById("layout");
+
+const createFloor = ({
+	floorNumber,
+	firstFloor = false,
+	lastFloor = false,
+}) => {
+	return `<div class="floor">
+				<div class="floor-layout">
+					${
+						!lastFloor
+							? `<div>
+						<button>Up</button>
+					</div>`
+							: ""
+					}
+					${
+						!firstFloor
+							? `<div>
+						<button>Down</button>
+					</div>`
+							: ""
+					}
+				</div>
+				<div class="floor-number">Floor ${floorNumber}</div>
+			</div>`;
+};
+
+const createLayout = (floors, lifts) => {
+	layout.innerHTML = "";
+	for (let i = floors; i >= 1; i--) {
+		if (i === 1) {
+			layout.innerHTML += createFloor({ floorNumber: i, firstFloor: true });
+		} else if (i === floors) {
+			layout.innerHTML += createFloor({ floorNumber: i, lastFloor: true });
+		} else {
+			layout.innerHTML += createFloor({ floorNumber: i });
+		}
+	}
+};
+
+const submitHandler = (e) => {
+	e.preventDefault();
+	const floors = Number(form[0].value);
+	const lifts = Number(form[1].value);
+	createLayout(floors, lifts);
+};
+
+form.addEventListener("submit", submitHandler);
