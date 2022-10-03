5a0d3150c46a881c24ea8111c1fe5295ca0ef5de
Nitin Kalra <nikkalra88@gmail.com>
2022-10-03 23:20:01 +0530

    update: milestone 1 done

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const form = document.getElementById("form");
 const layout = document.getElementById("layout");
+const lifts = document.getElementsByClassName('lift')
+
+// Check if a lift is already present at that floor, if yes, open doors
+// Find out the closest available lift
+// Call that lift
+
+let Alllifts = []
 
 const createFloor = ({
 	floorNumber,
@@ -11,14 +18,14 @@ const createFloor = ({
 					${
 						!lastFloor
 							? `<div>
-						<button>Up</button>
+						<button onclick="callLift(${floorNumber})">Up</button>
 					</div>`
 							: ""
 					}
 					${
 						!firstFloor
 							? `<div>
-						<button>Down</button>
+						<button onclick="callLift(${floorNumber})">Down</button>
 					</div>`
 							: ""
 					}
@@ -27,8 +34,37 @@ const createFloor = ({
 			</div>`;
 };
 
+const isLiftPresent = floor => Alllifts.filter(item=>item.currentFloor === floor).length !== 0
+
+const findClosestLift = floor => Alllifts.filter(lifts=>lifts.available).reduce((closestLift, currentLift) =>  Math.abs(closestLift.currentFloor - floor) <= Math.abs(currentLift.currentFloor - floor) ? closestLift:currentLift)
+
+const updateLifts = (liftNumber, newFloor) => {
+	console.log("lift position is being updated", lifts[liftNumber - 1]);
+	lifts[liftNumber - 1].style.left = liftNumber * 6+"rem" 
+	lifts[liftNumber - 1].style.bottom = (newFloor-1) * 6+"rem" 
+
+	Alllifts = Alllifts.map((lift) =>
+		lift.liftNumber === liftNumber
+			? { ...lift, currentFloor: newFloor }
+			: { ...lift }
+	);
+};
+
+const callLift = (floorNumber) =>{
+	if(isLiftPresent(floorNumber)){
+		// Open doors
+		console.log('opening door')
+	}
+	else{
+		const calledLift = findClosestLift(floorNumber)
+		console.log('This lift is being called', calledLift)
+		updateLifts(calledLift.liftNumber, floorNumber)
+	}
+}
+
 const createLayout = (floors, lifts) => {
 	layout.innerHTML = "";
+	console.log('layout created')
 	for (let i = floors; i >= 1; i--) {
 		if (i === 1) {
 			layout.innerHTML += createFloor({ floorNumber: i, firstFloor: true });
@@ -39,13 +75,13 @@ const createLayout = (floors, lifts) => {
 		}
 	}
 	for (let i = 0; i < lifts; i++) {
-		layout.children[
-			layout.children.length - 1
-		].children[0].innerHTML += `<div class="lift"></div>`;
+		Alllifts = [...Alllifts, {liftNumber: i+1, available: true, currentFloor: 1}]
+		layout.innerHTML += `<div style="left: ${(i+1)*6}rem; bottom: 0rem" class="lift"></div>`;
 	}
 };
 
 const submitHandler = (e) => {
+	console.log('request submitted')
 	e.preventDefault();
 	const floors = Number(form[0].value);
 	const lifts = Number(form[1].value);
