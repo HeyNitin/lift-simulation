359ddd7d92e7fe7689b159cb789f0b4c66635bd2
Nitin Kalra <nikkalra88@gmail.com>
2022-10-05 14:47:03 +0530

    feat: All functionalities done, css pending

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const form = document.getElementById("form");
 const layout = document.getElementById("layout");
 const lifts = document.getElementsByClassName("lift");
 
-// Check if a lift is already present at that floor, if yes, open doors
-// Find out the closest available lift
-// Call that lift
-
 let Alllifts = [];
+let remainingFloors = [];
 
 const createFloor = ({
 	floorNumber,
@@ -40,66 +37,87 @@ const isLiftPresent = (floor) =>
 const findLift = (liftNumber) =>
 	Alllifts.filter((item) => item.liftNumber === liftNumber)[0];
 
-const findClosestActiveLift = (floor) =>
-	Alllifts.filter((lift) => lift.available).reduce(
-		(closestLift, currentLift) =>
+const isReached = (liftNumber) =>
+	Alllifts.filter((item) => item.liftNumber === liftNumber)[0].reached;
+
+const findClosestActiveLift = (floor) => {
+	const filteredLifts = Alllifts.filter((lift) => lift.available);
+	if (filteredLifts.length) {
+		return filteredLifts.reduce((closestLift, currentLift) =>
 			Math.abs(closestLift.currentFloor - floor) <=
 			Math.abs(currentLift.currentFloor - floor)
 				? closestLift
 				: currentLift
-	);
-
+		);
+	} else {
+		return false;
+	}
+};
 const updateLifts = (liftNumber, newFloor) => {
-	const floorDifference = Math.abs(findLift(liftNumber).currentFloor - newFloor)
-	lifts[liftNumber - 1].style.transition = `bottom ${floorDifference * 2}s`
+	const floorDifference = Math.abs(
+		findLift(liftNumber).currentFloor - newFloor
+	);
+	lifts[liftNumber - 1].style.transition = `bottom ${floorDifference * 2}s`;
 	lifts[liftNumber - 1].style.left = liftNumber * 6 + "rem";
 	lifts[liftNumber - 1].style.bottom = (newFloor - 1) * 6 + "rem";
-	setTimeout(() => {
-		openDoors(liftNumber)
-		Alllifts = Alllifts.map((lift) =>
-			lift.liftNumber === liftNumber
-				? { ...lift, currentFloor: newFloor }
-				: { ...lift }
-		);		
-	}, 2000*floorDifference);
-	setTimeout(()=> makeLiftAvailable(liftNumber), (2000*floorDifference)+5000)
+	setTimeout(() => openDoors(liftNumber), 2000 * floorDifference);
+	setTimeout(
+		() => makeLiftAvailable(liftNumber, remainingFloors),
+		2000 * floorDifference + 5000
+	);
+	Alllifts = Alllifts.map((lift) =>
+		lift.liftNumber === liftNumber
+			? { ...lift, currentFloor: newFloor, reached: false }
+			: { ...lift }
+	);
 };
 
-const openDoors = liftNumber =>{
-	lifts[liftNumber - 1].children[0].style.left = '-50%'
-	lifts[liftNumber - 1].children[1].style.right = '-50%'
-	setTimeout(()=>{
-		lifts[liftNumber - 1].children[0].style.left = 0;
-		lifts[liftNumber - 1].children[1].style.right = 0;
-	},2500)
-}
+const openDoors = (liftNumber) => {
+	if (isReached) {
+		lifts[liftNumber - 1].children[0].style.width = "0%";
+		lifts[liftNumber - 1].children[1].style.width = "0%";
+		setTimeout(() => {
+			lifts[liftNumber - 1].children[0].style.width = "50%";
+			lifts[liftNumber - 1].children[1].style.width = "50%";
+		}, 2500);
+	}
+};
 
-const makeLiftUnavailable = liftNumber =>{
-	Alllifts.forEach(lift=> {
-		if(lift.liftNumber === liftNumber){
-			lift.available = false
-		}
-	})
-}
-
-const makeLiftAvailable = (liftNumber) => {
+const makeLiftUnavailable = (liftNumber) => {
 	Alllifts.forEach((lift) => {
 		if (lift.liftNumber === liftNumber) {
-			lift.available = true;
+			lift.available = false;
 		}
 	});
-}; 
+};
+
+const makeLiftAvailable = (liftNumber, remainingFloors) => {
+	if (remainingFloors.length) {
+		updateLifts(liftNumber, remainingFloors[0]);
+		remainingFloors.shift();
+	} else {
+		Alllifts.forEach((lift) => {
+			if (lift.liftNumber === liftNumber) {
+				lift.available = true;
+				lift.reached = true;
+			}
+		});
+	}
+};
 
 const callLift = (floorNumber) => {
-	const onFloorLift = isLiftPresent(floorNumber)
-	if (onFloorLift.length) {
-		makeLiftUnavailable(onFloorLift[0].liftNumber)
-		openDoors(onFloorLift[0].liftNumber);
-		setTimeout(() => makeLiftAvailable(onFloorLift[0].liftNumber), 5000);
-	} else {
-		const calledLift = findClosestActiveLift(floorNumber);
-		makeLiftUnavailable(calledLift.liftNumber)
-		updateLifts(calledLift.liftNumber, floorNumber);
+	let calledLift = isLiftPresent(floorNumber)[0];
+	if (calledLift?.reached !== false) {
+		console.log('ran')
+		if (!calledLift) {
+			calledLift = findClosestActiveLift(floorNumber);
+		}
+		if (calledLift) {
+			makeLiftUnavailable(calledLift.liftNumber);
+			updateLifts(calledLift.liftNumber, floorNumber);
+		} else {
+			remainingFloors = [...remainingFloors, floorNumber];
+		}
 	}
 };
 
@@ -117,7 +135,7 @@ const createLayout = (floors, lifts) => {
 	for (let i = 0; i < lifts; i++) {
 		Alllifts = [
 			...Alllifts,
-			{ liftNumber: i + 1, available: true, currentFloor: 1 },
+			{ liftNumber: i + 1, available: true, currentFloor: 1, reached: true },
 		];
 		layout.innerHTML += `<div style="left: ${
 			(i + 1) * 6
